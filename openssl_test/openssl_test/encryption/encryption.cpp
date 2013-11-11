956dea5467e611e81d1d55cbea7df4a9d0ae2169
cswuyg <cswuyg@gmail.com>
2013-11-11 17:58:05 +0800

    update encryption

--- openssl_test/openssl_test/encryption/encryption.cpp
+++ openssl_test/openssl_test/encryption/encryption.cpp
@@ -165,18 +165,32 @@ namespace Encryption
 		return strRet;
 	}
 
-	std::string EncodeAES( const std::string& strPassword, const std::string& strData )
+
+	std::string EncodeAES( const std::string& password, const std::string& data )
 	{
 		AES_KEY aes_key;
-		if(AES_set_encrypt_key((const unsigned char*)strPassword.c_str(), strPassword.length() * 8, &aes_key) < 0)
+		if(AES_set_encrypt_key((const unsigned char*)password.c_str(), password.length() * 8, &aes_key) < 0)
 		{
 			assert(false);
 			return "";
 		}
 		std::string strRet;
-		for(unsigned int i = 0; i < strData.length()/AES_BLOCK_SIZE; i++)
+		std::string data_bak = data;
+		unsigned int data_length = data_bak.length();
+		int padding = 0;
+		if (data_bak.length() % AES_BLOCK_SIZE > 0)
 		{
-			std::string str16 = strData.substr(i*AES_BLOCK_SIZE, AES_BLOCK_SIZE);
+			padding =  AES_BLOCK_SIZE - data_bak.length() % AES_BLOCK_SIZE;
+		}
+		data_length += padding;
+		while (padding > 0)
+		{
+			data_bak += "\0";
+			padding--;
+		}
+		for(unsigned int i = 0; i < data_length/AES_BLOCK_SIZE; i++)
+		{
+			std::string str16 = data_bak.substr(i*AES_BLOCK_SIZE, AES_BLOCK_SIZE);
 			unsigned char out[AES_BLOCK_SIZE];
 			AES_encrypt((const unsigned char*)str16.c_str(), out, &aes_key);
 			strRet += std::string((const char*)out, AES_BLOCK_SIZE);
